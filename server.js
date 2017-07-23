59f8fc628cb9325bea86d6cb69f10743d9e1a0b2
Oleg-Orekhov <orekhovoleg96@gmail.com>
2017-07-23 21:03:50 +0300

    commit6

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/res', express.static(__dirname + '/src'));
 app.use('/script', express.static(__dirname + '/node_modules'));
 
 app.get('*', function (request, response){
-    //response.sendFile(path.resolve(__dirname, '/src', 'index.html'));
+    response.sendFile('/src/index.html');
     response.send(path.resolve(__dirname));
 });
 
